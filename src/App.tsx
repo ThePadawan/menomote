d98dc3017480a2ea88637c5519afa5a4a120dcc2
Rudolf M. Schreier <thepadawan3@gmail.com>
2020-05-22 13:43:19 +0200

    Simplify CSS

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,22 @@ const click = (setFallbackUrl: (s: string | null) => void) => {
 const App = () => {
   const [fallbackUrl, setFallbackUrl] = useState<string | null>(null);
   return (
-    <div>
-      <button onClick={(_) => click(setFallbackUrl)}>
-        Generate sheet &amp; answer key
-      </button>
-      <div>
-        {fallbackUrl && (
-          <a href={fallbackUrl} download="svg.pdf">
-            File not downloading? Try this link instead.
-          </a>
-        )}
+    <>
+      <div className="App">
+        <div>
+          <button onClick={(_) => click(setFallbackUrl)}>
+            Generate sheet &amp; answer key
+          </button>
+        </div>
+        <div>
+          {fallbackUrl && (
+            <a href={fallbackUrl} download="svg.pdf">
+              File not downloading? Try this link instead.
+            </a>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
